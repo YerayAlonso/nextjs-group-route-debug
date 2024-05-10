218e6fb7e0752cdc36dbcf7ad8980d859c719c5a
Yeray Alonso <yeray@steema.com>
2024-05-10 14:40:02 +0200

    Add test pages to reproduce the both the working route and the broken route

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null;
+  return <>
+    <div><a href="/working">Working</a></div>
+    <div><a href="/broken">Broken</a></div>
+  </>;
 }
